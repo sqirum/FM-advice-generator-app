0d536f7a5be5a37b5b4b7d34939e97d923301b0c
sqirum <pauloslh94@gmail.com>
2022-10-11 22:24:26 +0100

    add app api call, update fn & event listeners

--- advices.js
+++
@@ -1,3 +0,0 @@
-const teste = 'advice loaded';
-
-export {teste}
--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-import {teste} from '/advices';
-console.log(teste);
-console.log('app loaded');
+
+const adviceId = document.querySelector('#adviceId');
+const quote = document.querySelector('#quote');
+const dice = document.querySelector('.dice-wrapper img');
+
+dice.addEventListener('click', () => {
+    getAdvice()
+        .then(data => updateUi(data))
+        .catch(err => console.log(err));
+})
+
+// * API CALL
+const getAdvice = async () => {
+    const base = 'https://api.adviceslip.com/advice';
+    
+    const response = await fetch(base);
+    
+    const data = await response.json();
+
+    return data
+}
+
+// * Update UI with data parameter received by any API call
+const updateUi = (data) => {
+    adviceId.innerHTML = data.slip.id;
+    quote.innerHTML = data.slip.advice;
+}
